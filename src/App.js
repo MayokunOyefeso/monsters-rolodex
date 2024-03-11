871acf580f7456a82713e16d8dcf20084f5b0287
Mayokun Oyefeso <mayokunoyefeso@gmail.com>
2024-03-11 12:39:41 -0500

    Add searchbox component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box component';
 
 class App extends Component{
   constructor() {
@@ -20,12 +20,13 @@ class App extends Component{
         return {monsters: users}
       }));
   }
+
   onSearchChange = (event) => {
     const searchString = event.target.value.toLocaleLowerCase();
     this.setState(() => {
       return { searchString }
     });
-    }
+  }
 
   render () {
     const { monsters, searchString } = this.state;
@@ -37,9 +38,7 @@ class App extends Component{
 
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' 
-        onChange={onSearchChange}
-        />
+        <SearchBox onChangeHandler = {onSearchChange} placeholder = 'search monsters' className='search-box'/>
         <CardList monsters={filteredMonsters} /> 
       </div>
     );
